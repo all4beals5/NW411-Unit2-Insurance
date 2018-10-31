00ffa61c0ed9f45c818a0663cc3bc3eca8d423dc
Brandi Beals <brandi.beals@gmail.com>
2018-10-31 17:48:59 -0500

    initial commit

---
+++ Beals Unit2 Analysis Code.R
@@ -0,0 +1,27 @@
+##################################################
+### Unit 2 Assignment - Insurance
+### MSDS 411-DL-56
+### Brandi Beals
+
+##################################################
+### Load packages
+
+
+##################################################
+### Set working directory & read data
+# home setwd("C:/Users/Brara/Dropbox/Masters in Predictive Analytics/411-DL-56/W-unit 2 weeks 4 to 6/Unit 2 - Insurance/4 Homework")
+setwd("C:/Users/bbeals/Dropbox (Personal)/Masters in Predictive Analytics/411-DL-56/W-unit 2 weeks 4 to 6/Unit 2 - Insurance/4 Homework")
+insurance=read.csv("logit_insurance",header=T)
+
+##################################################
+### Exploratory data analysis
+
+##################################################
+### Preparation and transformations
+
+##################################################
+### Model creation
+
+##################################################
+### Model selection
+
